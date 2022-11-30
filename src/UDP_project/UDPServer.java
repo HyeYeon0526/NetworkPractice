91a16220def24681ee2f472c12bfd60219b4cc42
HyeYeon0526 <hyk4238@naver.com>
2022-11-30 10:26:49 +0900

    UDP Practice

---
+++ src/UDP_project/UDPServer.java
@@ -0,0 +1,22 @@
+package UDP_project;
+
+import java.net.DatagramPacket; 
+import java.net.DatagramSocket; 
+public class UDPServer { 
+	public static void main(String[] args) { 
+		final int PORT = 7070; 
+		int count = 0; 
+		try (DatagramSocket socket = new DatagramSocket(PORT)) { 
+			byte[] data = new byte[512];
+			while (true) { 
+				DatagramPacket packet = new DatagramPacket(data, data.length); 
+				socket.receive(packet); 
+				String str = new String(packet.getData(), 0, packet.getLength(), "US-ASCII"); 
+				System.out.printf("%s %d %s %d\n", packet.getAddress(), packet.getPort(), str, ++count);
+				} 
+			}
+		catch (Exception e) { 
+			e.printStackTrace();
+		} 
+	} 
+}
